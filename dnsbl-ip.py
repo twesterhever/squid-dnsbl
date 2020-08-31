a764e5b84d089f4ee875a1f4179590f137c61efc
twesterhever <40121680+twesterhever@users.noreply.github.com>
2020-08-31 21:02:08 +0200

    dnsbl-ip.py: catch ValueError when resolving destinations
    This fixes a crash if the upstream or local DNS resolver returned
    invalid DNS replies to dnspython's resolver.

--- dnsbl-ip.py
+++ dnsbl-ip.py
@@ -106,7 +106,7 @@ def resolve_addresses(domain: str):
     try:
         for resolvedip in RESOLVER.query(domain, 'AAAA'):
             ip6a.append(str(resolvedip))
-    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers, dns.exception.Timeout):
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers, dns.exception.Timeout, ValueError):
         pass
 
     # Enumerate IPv4 addresses...
@@ -114,7 +114,7 @@ def resolve_addresses(domain: str):
     try:
         for resolvedip in RESOLVER.query(domain, 'A'):
             ip4a.append(str(resolvedip))
-    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers, dns.exception.Timeout):
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers, dns.exception.Timeout, ValueError):
         pass
 
     # Assemble all IP addresses and return them back
